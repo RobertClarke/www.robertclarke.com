8824cf6a20edbd677d92ce7cbe11b1c3b0acbd93
Robert Clarke <robert@rjfc.net>
2019-07-05 17:50:48 -0700

    changed s3 deploy url

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -6,5 +6,4 @@ aws cloudfront create-invalidation \
     --distribution-id E2NPH29TYNYP7 \
     --paths "/*"
 
-aws s3 sync ./public_html s3://www.robertclarke.com --delete
-
+aws s3 sync ./public_html/public s3://www.robertclarke.com --delete
